7417a975b4a19b2f1dceb7c616b18dbc5a2750ec
janhninini <janhvi1009@gmail.com>
2020-12-20 02:47:40 +0530

    df

--- guardians/views.py
+++ guardians/views.py
@@ -22,3 +22,43 @@ class IndexApi(APIView):
 
 
 
+
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+
+
+
+df = pd.read_csv("news.csv")
+
+x_train,x_test,y_train,y_test=train_test_split(df['text'], df['label'], test_size=0.2, random_state=7)
+
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+#DataFlair - Fit and transform train set, transform test set
+tfidf_train=tfidf_vectorizer.fit_transform(x_train)
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+
+
+#DataFlair - Initialize a PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+#DataFlair - Predict on the test set and calculate accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
+
+
+
+
+
+
+input_data = [a]
+vectorized_input_data = tfidf_vectorizer.transform(input_data)
+prediction = pac.predict(vectorized_input_data)
+print(prediction)
